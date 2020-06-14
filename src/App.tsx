323c7d1ea8b0d5e2d0ea693a906d9b72cc96888c
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-06-14 19:14:00 -0300

    Input & Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './routes';
@@ -10,9 +10,9 @@ const App: React.FC = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#312e38' }}>
         <Routes />
-      </View>
+      </SafeAreaView>
     </NavigationContainer>
   );
 };
