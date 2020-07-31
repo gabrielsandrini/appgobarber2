00ce37fad69622c7d2092a41d9951e5565c26638
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-07-31 03:24:06 -0300

    Dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -47,6 +47,7 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -63,6 +64,8 @@ const AuthProvider: React.FC = ({ children }) => {
     });
 
     const { token, user } = response.data;
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     await AsyncStorage.multiSet([
       ['@GoBarber:token', token],
