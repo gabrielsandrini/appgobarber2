7ffc56e8231978eb73cd3812a399b75fed3371b2
Gabriel Sandrini <gabriel.sandrini@outlook.com>
2020-08-02 00:40:09 -0300

    AppointmentCreated Screen

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 import api from '../../services/api';
